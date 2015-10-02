0a5722b4e0d91ad9f775e5f3926e7e02f0cbfb81
ayan some <ayansome1@gmail.com>
2015-10-02 15:53:56 +0530

    swipe refresh layout done

--- app/src/main/java/com/example/this_is_ayan/findmyadvocate/Fragments/CHomeScreenFragment.java
+++ app/src/main/java/com/example/this_is_ayan/findmyadvocate/Fragments/CHomeScreenFragment.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.example.this_is_ayan.findmyadvocate.Activities.PostCase;
 import com.example.this_is_ayan.findmyadvocate.Adapters.CardAdapter;
 import com.example.this_is_ayan.findmyadvocate.Objects.caseObject;
@@ -18,6 +20,7 @@ import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,15 +30,16 @@ public class CHomeScreenFragment extends Fragment
     RecyclerView mRecyclerView;
     RecyclerView.LayoutManager mLayoutManager;
     static RecyclerView.Adapter mAdapter;
+   public boolean datachanged;
 
-   // TextView tv;
+
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
 
         View view=inflater.inflate(R.layout.c_homescreenfragment,container,false);
-
         mRecyclerView = (RecyclerView)view.findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(getActivity());
@@ -43,15 +47,27 @@ public class CHomeScreenFragment extends Fragment
         mAdapter = new CardAdapter(getActivity(),getData());
         mRecyclerView.setAdapter(mAdapter);
 
-      /*  tv=(TextView)view.findViewById(R.id.tv);
-        tv.setOnClickListener(new View.OnClickListener() {
+
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_layout);
+
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.red, R.color.green, R.color.blue);
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onClick(View v)
-            {
-                mAdapter = new CardAdapter(getActivity(),getData());
+            public void onRefresh() {
+                mAdapter = new CardAdapter(getActivity(), getData());
                 mRecyclerView.setAdapter(mAdapter);
             }
-        });*/
+        });
+
+
+
+
+
+
+
+
 
         fab=(FloatingActionButton)view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -65,7 +81,14 @@ public class CHomeScreenFragment extends Fragment
         return  view;
     }
 
-    public static List<caseObject> getData()
+
+
+
+
+
+
+
+    public  List<caseObject> getData()
     {
         final List<caseObject> mItems = new ArrayList<>();
         ParseQuery<ParseObject> query = ParseQuery.getQuery("cases");
@@ -87,10 +110,14 @@ public class CHomeScreenFragment extends Fragment
                         cases.setCaseDescription(caseDescription);
                         mItems.add(cases);
                     }
+
                     mAdapter.notifyDataSetChanged();
+                    mSwipeRefreshLayout.setRefreshing(false);
+
                 }
             }
         });
+
         return mItems;
     }
 }
